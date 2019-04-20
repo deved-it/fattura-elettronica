fc0db72dece9fc4bc3ba02a1b2ad9d4c0b5ba9d7
Marco <mirc86@email.it>
2019-04-20 12:51:05 +0200

    BugFix - In caso di fattura verso PA la versione del file non dev'essere FPR12 MA FPA12 Aggiunto in DatiTrasmissione function tipoFattura per leggere il dato Progressivo invio con uniqid

--- src/FatturaElettronica.php
+++ src/FatturaElettronica.php
@@ -42,7 +42,7 @@ class FatturaElettronica implements XmlSerializableInterface
     public function toXmlBlock(\XMLWriter $writer)
     {
         $writer->startElementNS('p', 'FatturaElettronica', null);
-        $writer->writeAttribute('versione', 'FPR12');
+        $writer->writeAttribute('versione', $this->fatturaElettronicaHeader->datiTrasmissione->tipoFattura());
         $writer->writeAttributeNS('xmlns', 'ds', null, 'http://www.w3.org/2000/09/xmldsig#');
         $writer->writeAttributeNS('xmlns', 'p', null, 'http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2');
         $writer->writeAttributeNS('xmlns', 'xsi', null, 'http://www.w3.org/2001/XMLSchema-instance');
